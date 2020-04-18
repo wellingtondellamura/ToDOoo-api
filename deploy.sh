3d85a10523e7a3838b645d3a554e34821ed99972
Wellington Della Mura <wellington.dellamura@bitzen.com.br>
2020-04-18 16:17:44 -0300

    new composer

--- deploy.sh
+++ deploy.sh
@@ -104,10 +104,11 @@ if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
   exitWithMessageOnError "Kudu Sync failed"
 fi
 
-# 2. Install Composer modules 
+# 2. Install Composer modules
 if [ -e "$DEPLOYMENT_TARGET/composer.json" ]; then
   echo Running composer install
   cd "$DEPLOYMENT_TARGET"
+  wget -c https://getcomposer.org/composer.phar
   eval php composer.phar install
   exitWithMessageOnError "composer failed"
   cd - > /dev/null
